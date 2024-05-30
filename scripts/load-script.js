41aa2f025a8b87b13ccef050b1d48e4983cb386d
MrJ <144938009+MrJ-v2-718@users.noreply.github.com>
2024-05-30 11:35:23 -0500

    Add files via upload

---
+++ scripts/load-script.js
@@ -0,0 +1,17 @@
+/*
+    Student Name: MrJ
+    File Name: load-script.js
+    Date: 4/29/2024
+*/
+// Hide paragraphs when page loads
+$(document).ready(function() {
+  $("p1").hide("fast");
+});
+
+$(document).ready(function() {
+  $("p2").hide("fast");
+});
+
+$(document).ready(function() {
+  $("p3").hide("fast");
+});
---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+// Show paragraphs on button click
+$( "#radahn-btn" ).on( "click", function() {
+    $( "p1" ).show( "slow" );
+  });
+  
+  $( "#rykard-btn" ).on( "click", function() {
+    $( "p2" ).show( "slow" );
+  });
+
+  $( "#malenia-btn" ).on( "click", function() {
+    $( "p3" ).show( "slow" );
+  });
+
+  $( "#summary-btn" ).on( "click", function() {
+    $( "p1" ).show( "slow" );
+  });
+  
+  $( "#weaknesses-btn" ).on( "click", function() {
+    $( "p2" ).show( "slow" );
+  });
+
+  $( "#strategy-btn" ).on( "click", function() {
+    $( "p3" ).show( "slow" );
+  });
